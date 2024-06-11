e951c9c7725fcb7dbb55504c3c9c15fffe85b319
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2024-06-11 17:33:46 -0600

    Add CSV output

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # TMdecoder
+
+A program to decode and display Strateole2 Telelemetry Messages (TMs).
+
+There are two sample TMs inlcuded here which can be used to try out the application:
+
+- `TM.RS41.ready_tm`: An RS41 telemetry message
+- `TM.LPC.ready_tm`: An LPC telemetry message.
+
+The `.ready_tm` file extension is the convention that CNES uses for
+the files created by their ground system.
+
+The `.dump` files are the text output from a binary dump made
+with the `od` program. They are useful for diagnosing the internal
+format of the TM messages.
+
+# Installation
+
+```sh
+pip3 install xmltodict
+git clone https://github.com/MisterMartin/TMdecoder.git
+```
+
+## Test
+```sh
+cd TMdecoder
+python3 TMdecoder.py -o csv.txt TM.RS41.ready_tm
+```
+
+## Usage
+
+```python3 TMdecoder.h -h``` displays help:
+
+```text
+usage: TMdecoder [-h] [-o OUTFILE] [-q] filename
+
+Decode a LASP StratoCore TM message and produce CSV
+
+positional arguments:
+  filename              TM message file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OUTFILE, --outfile OUTFILE
+                        Save CSV to a file
+  -q, --quiet           Turn off printing
+```
--- TMdecoder.py
+++ TMdecoder.py
@@ -2,9 +2,10 @@ import csv
 import struct
 import xmltodict
 import sys
+import argparse
 
 class TMmsg:
-    def __init__(self, data_file:str):
+    def __init__(self, msg_file_name:str):
         '''
         Base class for Strateole2 TM message decoding
 
@@ -20,12 +21,17 @@ class TMmsg:
             tm_msg.TMxml()
             tm_msg.CRCxml()
         '''
-        with open(data_file, "rb") as binary_file:
+        with open(msg_file_name, "rb") as binary_file:
             data = binary_file.read()
 
         self.data = data
         self.bindata = self.binaryData()
-        self.csvheader = ''
+        # All of the decoded data samples. Each record
+        # is a dictionary.
+        self.records = []
+        
+    def allSamples(self):
+        return self.records
 
     def parse_TM_xml(self)->str:
         xml_txt = self.delimitedText(b'<TM>', b'</TM>')
@@ -92,18 +98,57 @@ class RS41msg(TMmsg):
     #    uint16_t pres; (pres*100)
     #    uint16_t error;
     #};
-    def __init__(self, data:bytes):
+    def __init__(self, msg_file_name:str):
         '''
         Initialize the object with the provided binary data.
 
         Args:
-            data (bytes): The binary data to initialize the object.
+            msg_file_name: The message file name.
 
         Returns:
             None
         '''
-        super().__init__(data)
+        super().__init__(msg_file_name)
         self.csv_header = 'valid,frame_count,air_temp_degC,humdity_percent,pres_mb,module_error'
+        self.records = self.allRS41samples()
+
+    def csvText(self)->list:
+        '''
+        Generate CSV text lines from the records.
+
+        Returns:
+            list: List of CSV text lines.
+
+        Args:
+            self: The instance containing records and a CSV header.
+        '''
+
+        csv_lines = [self.csv_header]
+        for r in self.records:
+            csv_line = \
+                f"{r['valid']},{r['frame_count']},{r['air_temp_degC']:0.2f},{r['humdity_percent']:0.2f},{r['pres_mb']:0.2f},{r['module_error']}"
+            csv_lines.append(csv_line)
+        return csv_lines
+
+    def printCsv(self):
+        '''
+        Prints the CSV text lines generated from the records.
+
+        Args:
+            self: The instance containing the CSV text lines to print.
+            
+        Returns:
+            None
+        '''
+
+        for r in self.csvText():
+            print(r)
+    
+    def saveCsv(self, out_filename:str)->None:
+        with open(out_filename, "w") as out_file:
+            for r in self.csvText():
+                out_file.write(r)
+                out_file.write('\n')
 
     def decodeRS41sample(self, record)->dict:
         '''
@@ -117,11 +162,11 @@ class RS41msg(TMmsg):
         '''
         r = {}
         r['valid'] = struct.unpack_from('B', record, 0)[0]
-        r['frame'] = struct.unpack_from('>l', record, 1)[0]
-        r['tdry'] = struct.unpack_from('>H', record, 5)[0]/100.0-100.0
-        r['humidity'] = struct.unpack_from('>H', record, 7)[0]/100.0
-        r['pres'] = struct.unpack_from('>H', record, 9)[0]/100.0
-        r['error'] = struct.unpack_from('>H', record, 11)[0]
+        r['frame_count'] = struct.unpack_from('>l', record, 1)[0]
+        r['air_temp_degC'] = struct.unpack_from('>H', record, 5)[0]/100.0-100.0
+        r['humdity_percent'] = struct.unpack_from('>H', record, 7)[0]/100.0
+        r['pres_mb'] = struct.unpack_from('>H', record, 9)[0]/100.0
+        r['module_error'] = struct.unpack_from('>H', record, 11)[0]
         #print(f"{valid}, {frame}, {tdry:0.2f}, {humidity:0.2f}, {pres:0.2f}, 0x{error:04x}")
         return r
 
@@ -151,18 +196,31 @@ class RS41msg(TMmsg):
         '''
         return  struct.unpack_from('>L', self.bindata, 0)[0]
 
+def argParse():
+    '''
+    Parse command line arguments for the TMdecoder script.
+
+    Returns:
+        Parsed command line arguments.
+
+    '''
+    parser = argparse.ArgumentParser(
+                        prog='TMdecoder',
+                        description='Decode a LASP StratoCore TM message and produce CSV',
+                        epilog='')
+    parser.add_argument('filename', help='TM message file')           
+    parser.add_argument('-o', '--outfile', help='Save CSV to a file')
+    parser.add_argument('-q', '--quiet',  action='store_true', help='Turn off printing')  # on/off flag
+    return parser.parse_args()
+
 if __name__ == "__main__":
 
+    args = argParse()
+    print(args)
+
     rs41_msg = RS41msg(sys.argv[1])
+    if not args.quiet:
+        rs41_msg.printCsv()
+    if args.outfile:
+        rs41_msg.saveCsv(args.outfile)
 
-    print(rs41_msg.parse_TM_xml())
-    print(rs41_msg.parse_CRC_xml())
-
-    time_stamp = rs41_msg.timeStamp()
-    print(f"timestamp: {time_stamp}")
-
-    records = rs41_msg.allRS41samples()
-    print(f"{len(records)} data records in the file")
-
-    for r in records:
-        print(r)
