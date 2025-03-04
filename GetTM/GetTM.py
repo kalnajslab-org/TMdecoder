5d1fa37c4b0b9a5ba642706d389904c61a3eb4f3
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2025-03-03 18:07:46 -0700

    Nice updates to ECUReport decoding
    - Use the current ECUReport revision
    - Use the excellent bitstruct dictionary decoding
    - Decode all records in the TM

--- GetTM/GetTM.py
+++ GetTM/GetTM.py
@@ -12,12 +12,14 @@ format_string = (
     'u11'  # v12 (11 bits)
     'u13'  # v56 (13 bits)
     'u11'  # board_t (11 bits)
+    'u8'   # switch_mA (8 bits)
     'u1'   # gps_valid (1 bit)
     's32'  # gps_lat (32 bits)
     's32'  # gps_lon (32 bits)
     'u16'  # gps_alt (16 bits)
     'u5'   # gps_sats (5 bits)
-    'u8'   # gps_hdop (8 bits)
+    'u17'  # gps_date (17 bits)
+    'u25'  # gps_time (25 bits)
     'u8'   # gps_age_secs (8 bits)
     'u1'   # rs41_valid (1 bit)
     'u14'  # rs41_airt (14 bits)
@@ -31,12 +33,31 @@ format_string = (
 )
 
 variable_names = [
-    'rev', 'heat_on', 'v5', 'v12', 'v56', 'board_t', 'gps_valid', 
-    'gps_lat', 'gps_lon', 'gps_alt', 'gps_sats', 'gps_hdop', 
-    'gps_age_secs', 'rs41_valid', 'rs41_airt', 'rs41_hum', 
-    'rs41_hst', 'rs41_pres', 'rs41_pcb_h', 'tsen_airt', 
-    'tsen_ptemp', 'tsen_pres'
-    ]
+    'rev', 
+    'heat_on', 
+    'v5', 
+    'v12', 
+    'v56', 
+    'board_t', 
+    'switch_mA', 
+    'gps_valid', 
+    'gps_lat', 
+    'gps_lon', 
+    'gps_alt', 
+    'gps_sats', 
+    'gps_date', 
+    'gps_time', 
+    'gps_age_secs', 
+    'rs41_valid', 
+    'rs41_airt', 
+    'rs41_hum', 
+    'rs41_hst', 
+    'rs41_pres', 
+    'rs41_pcb_h', 
+    'tsen_airt', 
+    'tsen_ptemp', 
+    'tsen_pres'
+]
 
 if __name__ == "__main__":
     # Open the file
@@ -52,61 +73,54 @@ if __name__ == "__main__":
         print('TM rec_size:', rec_size)
         print()
         
-        data_record = data_bytes[4:]
-    
-        # Unpack the first record
-        unpacked_data = unpack(format_string, data_record)
-        # print(unpacked_data)
+        record_num = 0
+        offset = 4
+        while offset < len(data_bytes):
+            if offset+rec_size > len(data_bytes):
+                # We've reached the terminating bytes at the end of the file
+                break
+            data_record = data_bytes[offset:offset+rec_size]
+        
+            # Unpack the unscaled parameters for the first record
+            vars = unpack_dict(format_string, variable_names, data_record)
 
-        # Scale the data
-        rev = unpacked_data[0]
-        heat_on = bool(unpacked_data[1])
-        v5 = unpacked_data[2]/100.0
-        v12 = unpacked_data[3]/100.0
-        v56 = unpacked_data[4]/100.0
-        board_t = unpacked_data[5]/10.0-100.0
-        gps_valid = bool(unpacked_data[6])
-        gps_lat = unpacked_data[7]*1.0e-6
-        gps_lon = unpacked_data[8]*1.0e-6
-        gps_alt = unpacked_data[9]*1.0
-        gps_sats = unpacked_data[10]
-        gps_hdop = unpacked_data[11]
-        gps_age_secs = unpacked_data[12]
-        rs41_valid = bool(unpacked_data[13])
-        rs41_airt = unpacked_data[14]/100.0-100.0
-        rs41_hum = unpacked_data[15]/100.0
-        rs41_hst = unpacked_data[16]
-        rs41_pres = unpacked_data[17]/100.0
-        rs41_pcb_h = bool(unpacked_data[18])
-        tsen_airt = unpacked_data[19]
-        tsen_ptemp = unpacked_data[20]
-        tsen_pres = unpacked_data[21]
+            # Scale them
+            scaled_vars = {}
+            scaled_vars['rev'] = vars['rev']
+            scaled_vars['heat_on'] = bool(vars['heat_on'])
+            scaled_vars['v5'] = vars['v5']/100.0
+            scaled_vars['v12'] = vars['v12']/100.0
+            scaled_vars['v56'] = vars['v56']/100.0
+            scaled_vars['board_t'] = vars['board_t']/10.0-100.0
+            scaled_vars['switch_mA'] = vars['switch_mA']
+            scaled_vars['gps_valid'] = bool(vars['gps_valid'])
+            scaled_vars['gps_lat'] = vars['gps_lat']*1.0e-6
+            scaled_vars['gps_lon'] = vars['gps_lon']*1.0e-6
+            scaled_vars['gps_alt'] = vars['gps_alt']*1.0
+            scaled_vars['gps_sats'] = vars['gps_sats']
+            scaled_vars['gps_date'] = vars['gps_date']
+            scaled_vars['gps_time'] = vars['gps_time']
+            scaled_vars['gps_age_secs'] = vars['gps_age_secs']
+            scaled_vars['rs41_valid'] = bool(vars['rs41_valid'])
+            scaled_vars['rs41_airt'] = vars['rs41_airt']/100.0-100.0
+            scaled_vars['rs41_hum'] = vars['rs41_hum']/100.0
+            scaled_vars['rs41_hst'] = vars['rs41_hst']
+            scaled_vars['rs41_pres'] = vars['rs41_pres']/100.0
+            scaled_vars['rs41_pcb_h'] = bool(vars['rs41_pcb_h'])
+            scaled_vars['tsen_airt'] = vars['tsen_airt']
+            scaled_vars['tsen_ptemp'] = vars['tsen_ptemp']
+            scaled_vars['tsen_pres'] = vars['tsen_pres']
 
+            print(f'----- Record {record_num}:')
+            record_num += 1
+            for key, value in scaled_vars.items():
+                if key in ['tsen_airt', 'tsen_ptemp', 'tsen_pres']:
+                    print(f'{key}: 0x{value:06x}')
+                elif key in ['gps_date']:
+                    print(f'{key}:   {value:06d}')
+                elif key in ['gps_time']:
+                    print(f'{key}: {value:08d}')
+                else:
+                    print(f'{key}: {value}')
+            offset += rec_size
 
-        # Print the scaled data
-        print('rev:', rev)
-        print('heat_on:', heat_on)
-        print('v5:', v5)
-        print('v12:', v12)
-        print('v56:', v56)
-        print('board_t:', board_t)
-        print('gps_valid:', gps_valid)
-        print('gps_lat:', gps_lat)
-        print('gps_lon:', gps_lon)
-        print('gps_alt:', gps_alt)
-        print('gps_sats:', gps_sats)
-        print('gps_hdop:', gps_hdop)
-        print('gps_age_secs:', gps_age_secs)
-        print('rs41_valid:', rs41_valid)
-        print('rs41_airt:', rs41_airt)
-        print('rs41_hum:', rs41_hum)
-        print('rs41_hst:', rs41_hst)
-        print('rs41_pres:', rs41_pres)
-        print('rs41_pcb_h:', rs41_pcb_h)
-        print('tsen_airt:', f'0x{tsen_airt:03x}')
-        print('tsen_ptemp:', f'0x{tsen_ptemp:06x}')
-        print('tsen_pres:', f'0x{tsen_pres:06x}')
-        print()
-
-        var_dict = unpack_dict(format_string, variable_names, data_record)
-        print(var_dict)
